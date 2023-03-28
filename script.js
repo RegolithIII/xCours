31ca6046d8c36391625f2492081391fc2f733108
RegolithIII <matvdlg@gmail.com>
2023-03-28 18:46:26 +0200

    Init

---
+++ script.js
@@ -0,0 +1,12 @@
+/* var isMenuShown = true;
+
+function menuShow() {
+    if (isMenuShown == true) {
+        document.getElementById("menu").style.display = 'none';
+        isMenuShown = false;
+    } else {
+        document.getElementById("menu").style.display = 'inline-block';
+        isMenuShown = true;
+    }
+}
+*/ 
