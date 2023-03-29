97ca88303c33bb7d50be54da0422ecf4676f0832
RegolithIII <matvdlg@gmail.com>
2023-03-29 17:10:19 +0200

    Changes to be committed: 	modified:   Cours/premiere/Anglais/Anglais.html 	modified:   Cours/premiere/Anglais/C1_Artificial_Intelligence.html 	modified:   Cours/premiere/Anglais/dystopia.html 	renamed:    Cours/premiere/Es.html -> Cours/premiere/Ens-Sci/Es.html 	renamed:    Cours/premiere/Espagnol.html -> Cours/premiere/Espagnol/Espagnol.html 	renamed:    "Cours/premiere/Franc\314\247ais.html" -> "Cours/premiere/Franc\314\247ais/Franc\314\247ais.html" 	renamed:    Cours/premiere/EMC.html -> Cours/premiere/HG/EMC.html 	renamed:    "Cours/premiere/Ge\314\201ographie.html" -> "Cours/premiere/HG/Ge\314\201ographie.html" 	renamed:    Cours/premiere/Histoire.html -> Cours/premiere/HG/Histoire.html 	modified:   Cours/premiere/Math/derive.html 	modified:   Cours/premiere/Math/exponentielle.html 	modified:   Cours/premiere/Math/fonction_derivee.html 	modified:   Cours/premiere/Math/math.html 	modified:   Cours/premiere/Math/polynomes.html 	modified:   "Cours/premiere/Math/probabilite\314\201s.html" 	modified:   Cours/premiere/Math/suites.html 	renamed:    Cours/premiere/SVT.html -> Cours/premiere/SVT/SVT.html 	modified:   index.html 	modified:   premiere.html 	modified:   script.js

--- script.js
+++ script.js
@@ -9,4 +9,21 @@ function menuShow() {
         isMenuShown = true;
     }
 }
-*/ 
+*/
+const template = document.createElement('template');
+
+template.innerHTML = `
+<header class="header">
+<a class="a" href="/index.html">Home</a>
+<a class="a" href="/Cours/premiere/Math/math.html">Maths</a>
+<a class="a" href="/Cours/premiere/Physique-Chimie/Physique-Chimie.html">Physique-Chimie</a>
+<a class="a" href="/Cours/premiere/SVT.html">SVT</a>
+<a class="a" href="#about">Ens. Scientifique</a>
+<a class="a" href="#about">Français</a>
+<a class="a" href="#about">Histoire-Géo</a>
+<a class="a" href="#about">Espagnol</a>
+<a class="a" href="#about">Anglais</a>
+</header>
+`;
+
+document.body.append(template.content);
