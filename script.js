be4be745175eac166a2664d9c0c8e9d1f5972b32
RegolithIII <matvdlg@gmail.com>
2023-03-29 17:41:52 +0200

    Changes : 	modified:   Cours/Seconde/seconde.html 	modified:   Cours/premiere/Anglais/Anglais.html 	modified:   Cours/premiere/Ens-Sci/Es.html 	modified:   Cours/premiere/Espagnol/Espagnol.html 	modified:   "Cours/premiere/Franc\314\247ais/Franc\314\247ais.html" 	deleted:    Cours/premiere/HG/EMC.html 	deleted:    "Cours/premiere/HG/Ge\314\201ographie.html" 	new file:   Cours/premiere/HG/Histoire-Geo.html 	deleted:    Cours/premiere/HG/Histoire.html 	modified:   Cours/premiere/Math/math.html 	modified:   Cours/premiere/Physique-Chimie/Physique-Chimie.html 	modified:   Cours/premiere/SVT/SVT.html 	modified:   script.js 	modified:   styles.css 	modified:   template.html

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ const template = document.createElement('template');
 
 template.innerHTML = `
 <header class="header">
-<a class="a" href="/index.html">Home</a>
+<a class="a" href="/index.html">Supercours</a>
 <a class="a" href="/Cours/premiere/Math/math.html">Maths</a>
 <a class="a" href="/Cours/premiere/Physique-Chimie/Physique-Chimie.html">Physique-Chimie</a>
-<a class="a" href="/Cours/premiere/SVT.html">SVT</a>
-<a class="a" href="#about">Ens. Scientifique</a>
-<a class="a" href="#about">Français</a>
-<a class="a" href="#about">Histoire-Géo</a>
-<a class="a" href="#about">Espagnol</a>
-<a class="a" href="#about">Anglais</a>
+<a class="a" href="/Cours/premiere/SVT/SVT.html">SVT</a>
+<a class="a" href="/Cours/premiere/Ens-Sci/Es.html">Ens. Scientifique</a>
+<a class="a" href="/Cours/premiere/Français/Français.html">Français</a>
+<a class="a" href="/Cours/premiere/HG/Histoire-Geo.html">Histoire-Géo</a>
+<a class="a" href="/Cours/premiere/Espagnol/Espagnol.html">Espagnol</a>
+<a class="a" href="/Cours/premiere/Anglais/Anglais.html">Anglais</a>
 </header>
 `;
 
