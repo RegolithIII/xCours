5e07ff1f9f4a276d7dab84f4a5eab0b4da3a2cd1
RegolithIII <matvdlg@gmail.com>
2023-04-03 21:52:26 +0200

    Modified: SuperCours/premiere/Math/derive.html SuperCours/premiere/Math/exponentielle.html SuperCours/premiere/Math/fonction_derivee.html "SuperCours/premiere/Math/probabilit\303\251s.html" SuperCours/premiere/Math/suites.html SuperCours/premiere/Math/trigonometrie.html cript.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ template.innerHTML = `
 <a class="a" href="/SuperCours/premiere/Physique-Chimie/Physique-Chimie.html">Physique-Chimie</a>
 <a class="a" href="/SuperCours/premiere/SVT/SVT.html">SVT</a>
 <a class="a" href="/SuperCours/premiere/Ens-Sci/Es.html">Ens. Scientifique</a>
-<a class="a" href="/SuperCours/premiere/Français/Français.html">Français</a>
+<a class="a" href="/SuperCours/premiere/Français/Français.html">Français</a>
 <a class="a" href="/SuperCours/premiere/HG/Histoire-Geo.html">Histoire-Géo</a>
 <a class="a" href="/SuperCours/premiere/Espagnol/Espagnol.html">Espagnol</a>
 <a class="a" href="/SuperCours/premiere/Anglais/Anglais.html">Anglais</a>
